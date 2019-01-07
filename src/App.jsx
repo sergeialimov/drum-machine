35b5f33f2efb25d6114bd821ba94f3d78f8381dc
Sergei Alimov <8912652@gmail.com>
2019-01-07 23:24:15 +0500

    add guitar sounds

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,79 @@ import './App.css';
 const sounds = {
   piano: [
     {
-      name: 'sound 1',
+      name: 'piano 1',
       path: '/sounds/piano/01.mp3',
     },
     {
-      name: 'sound 2',
+      name: 'piano 2',
       path: '/sounds/piano/02.mp3',
     },
     {
-      name: 'sound 3',
+      name: 'piano 3',
       path: '/sounds/piano/03.mp3',
     },
     {
-      name: 'sound 4',
+      name: 'piano 4',
       path: '/sounds/piano/04.mp3',
     },
     {
-      name: 'sound 5',
+      name: 'piano 5',
       path: '/sounds/piano/05.mp3',
     },
     {
-      name: 'sound 6',
+      name: 'piano 6',
       path: '/sounds/piano/06.mp3',
     },
     {
-      name: 'sound 7',
+      name: 'piano 7',
       path: '/sounds/piano/07.mp3',
     },
     {
-      name: 'sound 8',
+      name: 'piano 8',
       path: '/sounds/piano/08.mp3',
     },
     {
-      name: 'sound 9',
+      name: 'piano 9',
       path: '/sounds/piano/09.mp3',
     },
-  ]
+  ],
+  guitar: [
+    {
+      name: 'guitar 1',
+      path: '/sounds/guitar/01.mp3',
+    },
+    {
+      name: 'guitar 2',
+      path: '/sounds/guitar/02.mp3',
+    },
+    {
+      name: 'guitar 3',
+      path: '/sounds/guitar/03.mp3',
+    },
+    {
+      name: 'guitar 4',
+      path: '/sounds/guitar/04.mp3',
+    },
+    {
+      name: 'guitar 5',
+      path: '/sounds/guitar/05.mp3',
+    },
+    {
+      name: 'guitar 6',
+      path: '/sounds/guitar/06.mp3',
+    },
+    {
+      name: 'guitar 7',
+      path: '/sounds/guitar/07.mp3',
+    },
+    {
+      name: 'guitar 8',
+      path: '/sounds/guitar/08.mp3',
+    },
+    {
+      name: 'guitar 9',
+      path: '/sounds/guitar/09.mp3',
+    },
 };
 
 class App extends Component {
