ae64b842c856ce433d3e185eaa22fb85a6c07caf
Sergei Alimov <8912652@gmail.com>
2019-01-07 20:39:34 +0500

    add other sound names

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,89 @@
 import React, { Component } from 'react';
 import logo from './img/headphones.png';
 import './App.css';
-import sound1 from './sounds/piano/01.mp3'
-import sound2 from './sounds/piano/02.mp3'
-import sound3 from './sounds/piano/03.mp3'
-import sound4 from './sounds/piano/04.mp3'
-import sound5 from './sounds/piano/05.mp3'
-import sound6 from './sounds/piano/06.mp3'
-import sound7 from './sounds/piano/07.mp3'
-import sound8 from './sounds/piano/08.mp3'
-import sound9 from './sounds/piano/09.mp3'
+import soundQ from './sounds/piano/01.mp3'
+import soundW from './sounds/piano/02.mp3'
+import soundE from './sounds/piano/03.mp3'
+import soundA from './sounds/piano/04.mp3'
+import soundS from './sounds/piano/05.mp3'
+import soundD from './sounds/piano/06.mp3'
+import soundZ from './sounds/piano/07.mp3'
+import soundX from './sounds/piano/08.mp3'
+import soundC from './sounds/piano/09.mp3'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'test'
+      mode: 'piano',
+      name: '',
     }
     this.play = this.play.bind(this);
     this.onKeyPressed = this.onKeyPressed.bind(this);
     this.app = React.createRef();
-    this.q = React.createRef();
-    this.w = React.createRef();
-    this.e = React.createRef();
-    this.a = React.createRef();
-    this.s = React.createRef();
-    this.d = React.createRef();
-    this.z = React.createRef();
-    this.x = React.createRef();
-    this.c = React.createRef();
+    this.Q = React.createRef();
+    this.W = React.createRef();
+    this.E = React.createRef();
+    this.A = React.createRef();
+    this.S = React.createRef();
+    this.D = React.createRef();
+    this.Z = React.createRef();
+    this.X = React.createRef();
+    this.C = React.createRef();
   }
   
   // autotests for FreeCodeCamp
   componentDidMount () {
     const script = document.createElement("script");
-    script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
-    script.async = true;
+    script.Src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.Async = true;
     document.body.appendChild(script);
     this.focusDiv();
   }
   
-  play = (ref, soundName) => (e) => {
-    this.setState({ name: soundName })
-    ref.current.play();
+  play = (id) => (e) => {
+    console.log("id", id);
+    this.setState({ name: `sound${id}` })
+    this[id].current.play();
   }
 
   onKeyPressed(e) {
     switch (e.keyCode) {
       case 81:
-        this.q.current.currentTime = 0;
-        this.q.current.play();
+        this.Q.current.currentTime = 0;
+        this.Q.current.play();
         break;
       case 87:
-        this.w.current.currentTime = 0;
-        this.w.current.play();
+        this.W.current.currentTime = 0;
+        this.W.current.play();
         break;
       case 69:
-        this.e.current.currentTime = 0;
-        this.e.current.play();
+        this.E.current.currentTime = 0;
+        this.E.current.play();
         break;
       case 65:
-        this.a.current.currentTime = 0;
-        this.a.current.play();
+        this.A.current.currentTime = 0;
+        this.A.current.play();
         break;
       case 83:
-        this.s.current.currentTime = 0;
-        this.s.current.play();
+        this.S.current.currentTime = 0;
+        this.S.current.play();
         break;
       case 68:
-        this.d.current.currentTime = 0;
-        this.d.current.play();
+        this.D.current.currentTime = 0;
+        this.D.current.play();
         break;
       case 90:
-        this.z.current.currentTime = 0;
-        this.z.current.play();
+        this.Z.current.currentTime = 0;
+        this.Z.current.play();
         break;
       case 88:
-        this.x.current.currentTime = 0;
-        this.x.current.play();
+        this.X.current.currentTime = 0;
+        this.X.current.play();
         break;
       case 67:
-        this.c.current.currentTime = 0;
-        this.c.current.play();
+        this.C.current.currentTime = 0;
+        this.C.current.play();
         break;
     }
   }
@@ -99,32 +101,32 @@ class App extends Component {
       >
         <div id="drum-machine">
           <div id="display">
-            <div className="drum-pad" id="1" onClick={this.play(this.q, 'sound1')}>
-              <audio className="clip" id="Q" ref={this.q} src={sound1}/>Q
+            <div className="drum-pad" id="1" onClick={this.play('Q')}>
+              <audio className="clip" id="Q" ref={this.Q} src={soundQ}/>Q
             </div>
-            <div className="drum-pad" id="2" onClick={this.play(this.w, 'sound2')}>
-              <audio className="clip" id="W" ref={this.w} src={sound2}/>W
+            <div className="drum-pad" id="2" onClick={this.play('W')}>
+              <audio className="clip" id="W" ref={this.W} src={soundW}/>W
             </div>
-            <div className="drum-pad" id="3" onClick={this.play(this.e, 'sound3')}>
-              <audio className="clip" id="E" ref={this.e} src={sound3}/>E
+            <div className="drum-pad" id="3" onClick={this.play('E')}>
+              <audio className="clip" id="E" ref={this.E} src={soundE}/>E
             </div>
-            <div className="drum-pad" id="4" onClick={this.play(this.a, 'sound4')}>
-              <audio className="clip" id="A" ref={this.a} src={sound4}/>A
+            <div className="drum-pad" id="4" onClick={this.play('A')}>
+              <audio className="clip" id="A" ref={this.A} src={soundA}/>A
             </div>
-            <div className="drum-pad" id="5" onClick={this.play(this.s, 'sound5')}>
-              <audio className="clip" id="S" ref={this.s} src={sound5}/>S
+            <div className="drum-pad" id="5" onClick={this.play('S')}>
+              <audio className="clip" id="S" ref={this.S} src={soundS}/>S
             </div>
-            <div className="drum-pad" id="6" onClick={this.play(this.d, 'sound6')}>
-              <audio className="clip" id="D" ref={this.d} src={sound6}/>D
+            <div className="drum-pad" id="6" onClick={this.play('D')}>
+              <audio className="clip" id="D" ref={this.D} src={soundD}/>D
             </div>
-            <div className="drum-pad" id="7" onClick={this.play(this.z, 'sound7')}>
-              <audio className="clip" id="Z" ref={this.z} src={sound7}/>Z
+            <div className="drum-pad" id="7" onClick={this.play('Z')}>
+              <audio className="clip" id="Z" ref={this.Z} src={soundZ}/>Z
             </div>
-            <div className="drum-pad" id="8" onClick={this.play(this.x, 'sound8')}>
-              <audio className="clip" id="X" ref={this.x} src={sound8}/>X
+            <div className="drum-pad" id="8" onClick={this.play('X')}>
+              <audio className="clip" id="X" ref={this.X} src={soundX}/>X
             </div>
-            <div className="drum-pad" id="9" onClick={this.play(this.c, 'sound9')}>
-              <audio className="clip" id="C" ref={this.c} src={sound9}/>C
+            <div className="drum-pad" id="9" onClick={this.play('C')}>
+              <audio className="clip" id="C" ref={this.C} src={soundC}/>C
             </div>
           </div>
           <div id="rightPane">
