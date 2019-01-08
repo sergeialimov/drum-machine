d917cc0a9e26acc430664c163bc1d527ca365de5
Sergei Alimov <8912652@gmail.com>
2019-01-08 21:50:40 +0500

    move sounds info to external file

---
+++ src/soundInfo.js
@@ -0,0 +1,81 @@
+const sounds = {
+  piano: [
+    {
+      name: 'piano 1',
+      path: '/sounds/piano/01.mp3',
+    },
+    {
+      name: 'piano 2',
+      path: '/sounds/piano/02.mp3',
+    },
+    {
+      name: 'piano 3',
+      path: '/sounds/piano/03.mp3',
+    },
+    {
+      name: 'piano 4',
+      path: '/sounds/piano/04.mp3',
+    },
+    {
+      name: 'piano 5',
+      path: '/sounds/piano/05.mp3',
+    },
+    {
+      name: 'piano 6',
+      path: '/sounds/piano/06.mp3',
+    },
+    {
+      name: 'piano 7',
+      path: '/sounds/piano/07.mp3',
+    },
+    {
+      name: 'piano 8',
+      path: '/sounds/piano/08.mp3',
+    },
+    {
+      name: 'piano 9',
+      path: '/sounds/piano/09.mp3',
+    },
+  ],
+  guitar: [
+    {
+      name: 'guitar 1',
+      path: '/sounds/guitar/01.wav',
+    },
+    {
+      name: 'guitar 2',
+      path: '/sounds/guitar/02.wav',
+    },
+    {
+      name: 'guitar 3',
+      path: '/sounds/guitar/03.wav',
+    },
+    {
+      name: 'guitar 4',
+      path: '/sounds/guitar/04.wav',
+    },
+    {
+      name: 'guitar 5',
+      path: '/sounds/guitar/05.wav',
+    },
+    {
+      name: 'guitar 6',
+      path: '/sounds/guitar/06.wav',
+    },
+    {
+      name: 'guitar 7',
+      path: '/sounds/guitar/07.wav',
+    },
+    {
+      name: 'guitar 8',
+      path: '/sounds/guitar/08.wav',
+    },
+    {
+      name: 'guitar 9',
+      path: '/sounds/guitar/09.wav',
+    },
+  ]
+};
+
+
+export default sounds;
